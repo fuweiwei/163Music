324cb4355499f324b78c5c68442fdeb61660a3be
fuweiwei <fuweiwei@kjtpay.com.cn>
2017-02-24 16:04:23 +0800

    搭建框架、toolbar

--- app/src/main/java/com/veer/music/app/BaseActivity.java
+++ app/src/main/java/com/veer/music/app/BaseActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseActivity extends AppCompatActivity implements FragmentResultCallback {
+    protected BaseActivity mThis = this;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/veer/music/module/MainActivity.java
+++ app/src/main/java/com/veer/music/module/MainActivity.java
@@ -1,11 +1,9 @@
 package com.veer.music.module;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.veer.music.R;
 import com.veer.music.app.BaseActivity;
@@ -14,43 +12,26 @@ import com.veer.music.app.BaseActivity;
  *
  */
 public class MainActivity extends BaseActivity {
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
-        // Navigation Icon 要設定在 setSupoortActionBar 才有作用
-//        toolbar.setNavigationIcon(R.drawable.actionbar_menu);
-//        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+        initViews();
 
     }
 
-    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            String msg = "";
-            switch (menuItem.getItemId()) {
-                case R.id.action_discover:
-                    msg += "Click action_discover";
-                    break;
-                case R.id.action_music:
-                    msg += "Click action_music";
-                    break;
-                case R.id.action_friends:
-                    msg += "Click action_friends";
-                    break;
-            }
-
-            if(!msg.equals("")) {
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-    };
-
+    public void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(mThis, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();
+        drawerLayout.addDrawerListener(mDrawerToggle);
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // 為了讓 Toolbar 的 Menu 有作用，這邊的程式不可以拿掉
@@ -63,4 +44,5 @@ public class MainActivity extends BaseActivity {
 //
 //        return true;
 //    }
+    }
 }
