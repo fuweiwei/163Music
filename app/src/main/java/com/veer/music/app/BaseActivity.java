45c678a38fa7beff6d08b2a02f399107f6f5a7df
fuweiwei <fuweiwei@kjtpay.com.cn>
2017-02-24 15:38:54 +0800

    搭建框架、toolbar

--- app/src/main/java/com/veer/music/app/BaseActivity.java
+++ app/src/main/java/com/veer/music/app/BaseActivity.java
@@ -3,7 +3,7 @@ package com.veer.music.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * 基类  所有activity多必须继承
@@ -11,8 +11,7 @@ import android.support.v4.app.FragmentActivity;
  * date 17/2/23.
  */
 
-public class BaseActivity extends FragmentActivity implements FragmentResultCallback {
-
+public class BaseActivity extends AppCompatActivity implements FragmentResultCallback {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/veer/music/module/MainActivity.java
+++ app/src/main/java/com/veer/music/module/MainActivity.java
@@ -1,7 +1,11 @@
 package com.veer.music.module;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.veer.music.R;
 import com.veer.music.app.BaseActivity;
@@ -15,5 +19,48 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        // Navigation Icon 要設定在 setSupoortActionBar 才有作用
+//        toolbar.setNavigationIcon(R.drawable.actionbar_menu);
+//        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+
     }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_discover:
+                    msg += "Click action_discover";
+                    break;
+                case R.id.action_music:
+                    msg += "Click action_music";
+                    break;
+                case R.id.action_friends:
+                    msg += "Click action_friends";
+                    break;
+            }
+
+            if(!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // 為了讓 Toolbar 的 Menu 有作用，這邊的程式不可以拿掉
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        return true;
+//    }
 }
